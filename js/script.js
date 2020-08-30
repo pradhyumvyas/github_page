b88a45b9ce95b5e36d37ea3bd45e6e56fc59d289
pradhyumvyas <pradhyumvyas92@gmail.com>
2020-08-31 02:44:36 +0530

    Alignment

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var myHeader = document.querySelector(".header")
 
-myHeader.addEventListener("mouseenter", () =>{
+myHeader.addEventListener("mouseenter", () => {
     myHeader.style.fontSize = '50px';
     myHeader.style.color = "black";
 })
 
-myHeader.addEventListener("mouseleave", () =>{
+myHeader.addEventListener("mouseleave", () => {
     myHeader.style.fontSize = 'xx-large';
 })
